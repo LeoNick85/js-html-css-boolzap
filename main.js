e698b100fae11c2cbd081a0f0074af5c55f4652b
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-05-12 10:37:05 +0200

    Fixed error in message added

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//SCRIPT PER RICERCA CONTATTI
+
+
+//SCRIPT PER MESSAGGI
+//Creo una variabile con le possibili risposte dell'interlocutore in un array
+var chat_answers = ["Ok","Non saprei","Sono d'accordo","Non penso proprio","Credo che la risposta sia 42"];
+
 //Modifico il pulsante da microfono a Invia se la casella di testa viene attivata
 $("#write-text-box").focus(function(){
     $(".text-box .fa-microphone").hide();
@@ -14,25 +21,24 @@ $("#write-text-box").keydown(function(event){
 //Aggiungo un messaggio alla chat quando viene premuto il bottone invio
 $(".text-box .fa-paper-plane").click(addTextInChat);
 
-//Creo una variabile con le possibili risposte dell'interlocutore in un array
-var chat_answers = ["Ok","Non saprei","Sono d'accordo","Non penso proprio"];
-
 //FUNZIONI
 //Funzione per inserire testo nella chat
 function addTextInChat() {
-    var elementToAdd = $(".template .chat-right").clone();
+    var elementToAdd = $(".template .chat-utente").clone();
     var messageToAdd = $("#write-text-box").val();
 
     //Preparo il messaggio con HTML e lo aggiungo alla chat
-    var finalMessage = elementToAdd.text(messageToAdd);
-    finalMessage.appendTo("#container-chat-box");
+    elementToAdd.find("p:first-child").text(messageToAdd);
+    elementToAdd.addClass("chat-right");
+    elementToAdd.appendTo("#container-chat-box");
 
     //Prendo un messaggio casuale dall'array delle risposte e lo aggiungo come risposta dell'altro utente, con un ritardo di 1 minuto
     var random_answer = chat_answers[getRandom(0, (chat_answers.length - 1))];
-    var answerToAdd =  $(".template .chat-left").clone();
-    var finalAnswer = answerToAdd.text(random_answer);
+    var answerToAdd =  $(".template .chat-utente").clone();
+    answerToAdd.find("p:first-child").text(random_answer);
+    answerToAdd.addClass("chat-left");
     setTimeout (function(){
-        finalAnswer.appendTo("#container-chat-box");
+        answerToAdd.appendTo("#container-chat-box");
     }, 1000);
 
     //Ripristino il pulsante microfono fino a prossimo focus su box
