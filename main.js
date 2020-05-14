e7efcad1bb05d22031b49a081ea59af1444aade5
Daniele Nicolini <dnicolini.fusiorari@gmail.com>
2020-05-14 16:28:19 +0200

    Chat Version 2.1

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ var conversation_log = [
     ["Sì, mi sono arrivati ieri", "July 21, 1983 01:15:00",".chat-left"],
     ["Hai preparato tutto? Ci vediamo all'ora prevista al Reattore?", "July 21, 1983 01:15:00",".chat-right"],
     ["Sì, tutto pronto", "July 21, 1983 01:15:00",".chat-left"],
-    ["Ok, ci vediamo al Reattore", "July 21, 1983 01:15:00",".chat-left"]
+    ["Ok, ci vediamo al Reattore", "July 21, 1983 01:19:00",".chat-left"]
 ],
 //Seconda Chat(Lara Croft)
 [
@@ -49,7 +49,7 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Hai aperto al maggiordomo Smith?", "July 21, 1983 01:15:00",".chat-left"]
+    ["Hai aperto al maggiordomo Smith?", "July 21, 1983 03:15:00",".chat-left"]
 ],
 //Terza Chat(Minsc)
 [
@@ -57,7 +57,7 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Hai visto Boo?", "July 21, 1983 01:15:00",".chat-left"]
+    ["Hai visto Boo?", "July 21, 1983 22:15:00",".chat-left"]
 ],
 //Quarta Chat(Solid Snake)
 [
@@ -65,7 +65,7 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Kept you waiting?", "July 21, 1983 01:15:00",".chat-left"]
+    ["Kept you waiting?", "July 21, 1983 01:21:00",".chat-left"]
 ],
 //Quinta Chat(Kazuma Kiryu)
 [
@@ -73,7 +73,7 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Ti aspetto a Kamurocho", "July 21, 1983 01:15:00",".chat-left"]
+    ["Ti aspetto a Kamurocho", "July 21, 1983 01:55:00",".chat-left"]
 ],
 //Sesta Chat(Yennifer di Vengerberg)
 [
@@ -81,7 +81,7 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Aspetto il profumo di uva spina", "July 21, 1983 01:15:00",".chat-left"]
+    ["Aspetto il profumo di uva spina", "July 21, 1983 21:15:20",".chat-left"]
 ],
 //Settima Chat(Mordin Solus)
 [
@@ -89,12 +89,12 @@ var conversation_log = [
     ["Sì, ci troviamo lì", "July 21, 1983 01:15:00",".chat-left"],
     ["Va bene, allora parto da casa adesso", "July 21, 1983 01:15:00",".chat-right"],
     ["OK", "July 21, 1983 01:15:00",".chat-left"],
-    ["Ho finito la ricerca", "July 21, 1983 01:15:00",".chat-left"]
+    ["Ho finito la ricerca", "July 21, 1983 03:35:33",".chat-left"]
 ]
 ];
 
 //Ad avvio pagina genero la lista contatti, carico il contenuto della prima chat e la segno attiva nella lista dei CONTATTI
-printAccounts(account_list, conversation_log);
+printAccounts();
 $("#chat-item-container .chat-item:first-child").addClass("current");
 var nth_current_chat = 0;
 printChat(nth_current_chat);
@@ -119,7 +119,7 @@ $(document).on("click", ".chat-item", function(){
         $("#container-chat-box").html("");
 
         //Creo un ciclo for per ripubblicare in sequenza tutti gli elementi nella conversazione
-        nth_current_chat = $(this).index();
+        nth_current_chat = $(this).attr("data-id");
 
         printChat(nth_current_chat);
     }
@@ -172,6 +172,8 @@ $(document).on("click", ".message-hidden-menu p:nth-child(2)", function(){
     //Svuoto la chat e ricarico gli elementi della pagina dopo la cancellazione
     $("#container-chat-box").html("");
     printChat(nth_current_chat);
+    printAccounts();
+    $(".chat-item[data-id=" + nth_current_chat + "]").addClass("current");
 });
 
 //FUNZIONI
@@ -207,7 +209,7 @@ function addTextInChat() {
         var arrayAnswerToAdd = [random_answer, d, ".chat-left"];
         conversation_log[nth_current_chat].push(arrayAnswerToAdd);
         //Aggiorno la lista contatti per avere la preview dei messaggi corretta
-        printAccounts(account_list, conversation_log);
+        printAccounts();
         // //Riaggiungo la classe active alla chat corrente
         $(".chat-item[data-id=" + nth_current_chat + "]").addClass("current");
     }, 1000);
@@ -242,8 +244,30 @@ function getHHMM(actual_date) {
 function printAccounts() {
     //Svuoto l'elenco dei CONTATTI
     $("#chat-item-container").text("");
-    //Stampo di nuovo la lista
-    for (var i = 0; i < account_list.length; i++) {
+
+    //Stampo il primo elemento della lista
+    //Clono l'elemento account dai template e lo aggiorno col nome e l'immagine corretta
+    var accountToAdd = $(".template .chat-item").clone();
+    accountToAdd.find(".preview-name p").text(account_list[0][0]);
+    accountToAdd.find(".portrait img").attr("src", account_list[0][1]);
+    accountID = account_list[0][2];
+    accountToAdd.attr("data-id",accountID);
+
+    //Aggiungo come preview l'ultimo messaggio pubblicato
+    var total_messages = (conversation_log[0].length) - 1;
+    accountToAdd.find(".preview-message p").text(conversation_log[accountID][total_messages][0]);
+    //Metto l'ora dell'ultimo messaggio pubblicato
+    var lastMessageTime = new Date(conversation_log[accountID][total_messages][1]);
+
+    var timeText = getHHMM(lastMessageTime);
+    accountToAdd.find(".preview-time p").text(timeText);
+
+    //Aggiungo il contatto nel box degli account
+    accountToAdd.appendTo("#chat-item-container");
+
+
+    //Stampo di nuovo la lista a partire dal secondo elemento ordinando gli elementi in base al tempo dell'ultimo messaggio inserito
+    for (var i = 1; i < account_list.length; i++) {
         //Clono l'elemento account dai template e lo aggiorno col nome e l'immagine corretta
         var accountToAdd = $(".template .chat-item").clone();
         accountToAdd.find(".preview-name p").text(account_list[i][0]);
@@ -259,8 +283,45 @@ function printAccounts() {
         var timeText = getHHMM(lastMessageTime);
         accountToAdd.find(".preview-time p").text(timeText);
 
-        //Aggiungo il contatto nel box degli account
+        //Aggiungo il contatto nel box degli account nella giusta posizione
+        //Calcolo il contatore orario del messaggio corrente
+        var current_messageCounter = parseInt(lastMessageTime.getFullYear().toString() + lastMessageTime.getMonth().toString() + lastMessageTime.getDate().toString() + lastMessageTime.getHours().toString() + lastMessageTime.getMinutes().toString()+ lastMessageTime.getSeconds().toString());
+        console.log(current_messageCounter);
         accountToAdd.appendTo("#chat-item-container");
+
+
+        //Uso un ciclo per confrontare il tempo con gli elementi già pubblicati, se il messaggio è più recente pubblico in quella posizione
+        // var nth_account_printed = $("#chat-item-container .chat-item").length;
+        // console.log(nth_account_printed);
+        // var confrontCounter = 0;
+        // do {
+        //     //Seleziono l'elemento da confrontare con l'orario
+        //     var confrontingElement = $("#chat-item-container .chat-item").eq(confrontCounter);
+        //     var confrontingElementID = parseInt(confrontingElement.attr("data-id"));
+        //     var confrontingElementNthMessages = (conversation_log[confrontingElementID].length) - 1;
+        //     var confrontingElementDate = new Date(conversation_log[confrontingElementID][confrontingElementNthMessages][1]);
+        //     console.log(confrontingElementID);
+        //     console.log(confrontingElement);
+        //
+        //     var timeToCheck = parseInt(confrontingElementDate.getFullYear().toString() + confrontingElementDate.getMonth().toString() + confrontingElementDate.getDate().toString() + confrontingElementDate.getHours().toString() + confrontingElementDate.getMinutes().toString()+ confrontingElementDate.getSeconds().toString());
+        //     console.log(timeToCheck);
+        //
+        //     if(current_messageCounter > timeToCheck) {
+        //         console.log(current_messageCounter, timeToCheck, "più recente");
+        //         accountToAdd.insertBefore("#chat-item-container .chat-item:nth-child("+ confrontCounter +")");
+        //         break;
+        //     }
+
+            // if (current_messageCounter > timeToCheck) {
+            //     accountToAdd.insertBefore(".chat-item:nth-child("+ confrontCounter +")");
+            //     console.log("aggiungo");
+            //     break;
+            // } else if (current_messageCounter < timeToCheck) {
+            //     confrontCounter++;
+            //     console.log("riprovo");
+            // }
+        // } while ((confrontCounter >= nth_account_printed) && (current_messageCounter < timeToCheck));
+
     }
 }
 
